2849013a8f88b98d9497d009b4fdf7171a7ab65d
Jake Brucks <jakobpbrucks@gmail.com>
2020-05-29 16:16:06 -0500

    updated code, added output

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ca56dcf5a1aa644152cbe587b9ad33e3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBTJ6Udj3Wz7oM_4ApXrENxctSl5Y87inM"
